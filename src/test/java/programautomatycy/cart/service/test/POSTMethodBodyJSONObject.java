584c0ab94be5665416a9409ec3f7c008230e578d
elciak82@gmail.com <elciak82@gmail.com>
2023-07-03 14:47:56 +0200

    POSTMethodBodyJSONObject

--- src/test/java/programautomatycy/cart/service/test/POSTMethodBodyJSONObject.java
+++ src/test/java/programautomatycy/cart/service/test/POSTMethodBodyJSONObject.java
@@ -1,11 +1,33 @@
 package programautomatycy.cart.service.test;
 
+import helpers.ServiceHelper;
+import org.json.JSONObject;
 import org.junit.Test;
 
 public class POSTMethodBodyJSONObject {
 
+    private final ServiceHelper serviceHelper = new ServiceHelper();
+
     @Test
     public void addItemWithBodyJSONObject(){
+        JSONObject bodyRequest = new JSONObject();
+        bodyRequest.put("product_id", 140);
+        bodyRequest.put("quantity", 5);
+        bodyRequest.put("return_cart", true);
 
+        String endpoint = "/cocart/v1/add-item";
+
+        serviceHelper.sendPostRequest(bodyRequest.toString(), endpoint);
     }
+
+    @Test
+    public void calculateWithBodyJSONObject(){
+        JSONObject bodyRequest = new JSONObject();
+        bodyRequest.put("return", true);
+
+        String endpoint = "/cocart/v1/calculate";
+
+        serviceHelper.sendPostRequest(bodyRequest.toString(), endpoint);
+    }
+
 }
