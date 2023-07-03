a316ba552259c9254655f72c2c0f0f06ed19b2fa
elciak82@gmail.com <elciak82@gmail.com>
2023-07-03 12:07:07 +0200

    GetPropertyValues added

--- src/main/java/helpers/ServiceHelper.java
+++ src/main/java/helpers/ServiceHelper.java
@@ -1,9 +1,17 @@
 package helpers;
 
 import io.restassured.response.Response;
+import utils.GetPropertyValues;
+
 import static io.restassured.RestAssured.*;
 
 public class ServiceHelper {
+
+    GetPropertyValues getPropertyValues = new GetPropertyValues();
+
+    private final String LOGIN = getPropertyValues.getPropValue("login");
+    private final String PASSWORD = getPropertyValues.getPropValue("password");
+    private final String BASE_URI = getPropertyValues.getPropValue("shopServerUrl");
 
     public void addItem(Integer productId, Integer quantity, Boolean returnCart, String endpoint){
         Response response = given()
