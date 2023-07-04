d8cbf75f90296163239dd59e46ee8a55154eb7fb
elciak82@gmail.com <elciak82@gmail.com>
2023-07-04 15:34:10 +0200

    useResponseToUpdateItem

---
+++ src/test/java/programautomatycy/cart/service/test/UseValueFromResponse.java
@@ -0,0 +1,38 @@
+package programautomatycy.cart.service.test;
+
+import helpers.ServiceHelper;
+import io.restassured.response.Response;
+import org.json.JSONObject;
+import org.junit.Test;
+
+public class UseValueFromResponse {
+
+    private final ServiceHelper serviceHelper = new ServiceHelper();
+
+    @Test
+    public void useResponseToUpdateItem(){
+        JSONObject bodyRequest1 = new JSONObject();
+        bodyRequest1.put("product_id", 142);
+        bodyRequest1.put("quantity", 1);
+        bodyRequest1.put("return_cart", false);
+
+        String endpoint1 = "/cocart/v1/add-item";
+        Response response1 = serviceHelper.sendPostRequest(bodyRequest1.toString(), endpoint1);
+
+        String jsonString = response1.getBody().print();
+        JSONObject obj = new JSONObject(jsonString);
+
+        String key = obj.getString("key");
+        System.out.println(key);
+
+        JSONObject bodyRequest2 = new JSONObject();
+        bodyRequest2.put("cart_item_key", key);
+        bodyRequest2.put("quantity", 10);
+
+        String endpoint2 = "/cocart/v1/item";
+        Response response2 = serviceHelper.sendPostRequest(bodyRequest2.toString(), endpoint2);
+
+        response2.getBody().prettyPrint();
+
+    }
+}
